018ef2ee2cba695aff0df4314da870e0bf88c1a8
Cody Hutchens <pair+chutchens@humana.com>
2016-12-16 15:58:53 -0500

    Removed arrow operator from index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var binder = require('./binder.js');
 var autoBinder = require('./auto-binder.js');
 
-module.exports = (init) => {
+module.exports = function(init) {
     global.binder = global.binder == null ? new binder() : global.binder;
     if (init) {
         global.binder.bindAll(init);
